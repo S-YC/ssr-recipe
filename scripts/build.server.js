fc5ce50287c8e7fa2fc9e8a46088b944c893337a
S-YC <s9292909@gmail.com>
2022-06-14 23:31:03 +0900

    서버사이드 빌드 스크립트 작성

--- scripts/build.server.js
+++ scripts/build.server.js
@@ -10,8 +10,21 @@ const fs = require("fs-extra");
 const webpack = require("webpack");
 const config = require("../config/webpack.config.server");
 const paths = require("../config/paths");
+const resolve = require("resolve");
 
 function build() {
   console.log("Creating server build...");
   fs.emptyDirSync(paths.ssrBuild);
+  let compiler = webpack(config);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(stats.toString());
+    });
+  });
 }
+
+build();
