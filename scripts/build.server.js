569003c217be9901e40173fef0187b457137de08
S-YC <s9292909@gmail.com>
2022-06-14 23:26:43 +0900

    서버사이드 빌드 스크립트 작성

---
+++ scripts/build.server.js
@@ -0,0 +1,17 @@
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
+
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
+
+require("../config/env");
+const fs = require("fs-extra");
+const webpack = require("webpack");
+const config = require("../config/webpack.config.server");
+const paths = require("../config/paths");
+
+function build() {
+  console.log("Creating server build...");
+  fs.emptyDirSync(paths.ssrBuild);
+}
